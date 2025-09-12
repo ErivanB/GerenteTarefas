a0249d1f2ab7491e327ce86355bcd2f25e1cd327
Erivan Barros <ErivanB53@gmail.com>
2025-09-12 10:30:18 -0300

    Initial commit

---
+++ src/main/java/br/edu/ifpb/GerenteTarefas/GerenteTarefasApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.GerenteTarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenteTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenteTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifpb/GerenteTarefas/GerenteTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.GerenteTarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenteTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
