61e85fcf81ce83a7e7e822c9f9a28fc93879576a
Erivan Barros <ErivanB53@gmail.com>
2025-09-12 10:30:18 -0300

    Initial commit

---
+++ src/main/java/br/edu/ifpb/GerenteTarefas/controller/TaskController.java
@@ -0,0 +1,57 @@
+package br.edu.ifpb.GerenteTarefas.controller;
+
+import br.edu.ifpb.GerenteTarefas.model.Task;
+import br.edu.ifpb.GerenteTarefas.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
+        return taskRepository.findById(id)
+                .map(task -> ResponseEntity.ok().body(task))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task taskDetails) {
+        return taskRepository.findById(id)
+                .map(task -> {
+                    task.setTitle(taskDetails.getTitle());
+                    task.setDescription(taskDetails.getDescription());
+                    task.setCompleted(taskDetails.isCompleted());
+                    Task updatedTask = taskRepository.save(task);
+                    return ResponseEntity.ok().body(updatedTask);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteTask(@PathVariable Long id) {
+        return taskRepository.findById(id)
+                .map(task -> {
+                    taskRepository.delete(task);
+                    return ResponseEntity.ok().build();
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
---
+++ src/main/java/br/edu/ifpb/GerenteTarefas/repository/TaskRepository.java
@@ -0,0 +1,11 @@
+package br.edu.ifpb.GerenteTarefas.repository;
+
+
+
+import br.edu.ifpb.GerenteTarefas.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
